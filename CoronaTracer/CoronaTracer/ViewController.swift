3eb18e1801f37d47e968f4e3940f52b60fecbdee
Oleh Kudinov <oleh.kudinov@revolut.com>
2020-03-28 11:18:30 +0000

    Initial commit

---
+++ CoronaTracer/CoronaTracer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaTracer
+//
+//  Created by Oleh Kudinov on 28/03/2020.
+//  Copyright © 2020 CoronaTracer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
