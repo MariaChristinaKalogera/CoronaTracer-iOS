7da1f1e390fd671b96f68052b2445943ab1418b2
Oleh Kudinov <oleh.kudinov@revolut.com>
2020-03-28 13:39:55 +0000

    Show login screen

---
+++ CoronaTracer/Views/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  IntialViewController.swift
+//  CoronaTracer
+//
+//  Created by Oleh Kudinov on 28/03/2020.
+//  Copyright © 2020 CoronaTracer. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func login(_ sender: Any) {
+    }
+}
+
+
+
